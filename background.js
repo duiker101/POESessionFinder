8cd46a5296208214c09f98578f28866db1ce1b56
Simone Masiero <me@duiker101.net>
2017-05-14 17:12:05 +0200

    added missing id?

--- background.js
+++ background.js
@@ -1,26 +1,32 @@
 if (!chrome.cookies) {
-  chrome.cookies = chrome.experimental.cookies;
+    chrome.cookies = chrome.experimental.cookies;
 }
 
 function copyToClipboard(text) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val(text).select();
-  document.execCommand("copy");
-  $temp.remove();
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
 }
 
 function copyID() {
-  chrome.cookies.get({'name':'POESESSID','url':"https://pathofexile.com"},function(a){
-    copyToClipboard(a.value)
-  });
+    chrome.cookies.get({'name': 'POESESSID', 'url': "https://pathofexile.com"}, function (a) {
+        copyToClipboard(a.value)
+    });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  copyID();
+chrome.browserAction.onClicked.addListener(function (tab) {
+    copyID();
 });
 
-var context ="page"; 
+var context = "page";
 var title = "Copy Session ID";
-var id = chrome.contextMenus.create({"documentUrlPatterns":["*://*.pathofexile.com/*"],"title": title, "contexts":[context], "onclick": copyID});
+var id = chrome.contextMenus.create({
+    "id": "poe_sess_id_finder_1",
+    "documentUrlPatterns": ["*://*.pathofexile.com/*"],
+    "title": title,
+    "contexts": [context],
+    "onclick": copyID
+});
 
